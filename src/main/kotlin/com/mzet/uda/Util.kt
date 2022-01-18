66e9844ffdddcb1540dda7073360ad3c9cf4673d
McCreeMainer <odaving111@gmail.com>
2022-01-18 11:52:41 +0300

    Help resolved

--- src/main/kotlin/com/mzet/uda/Util.kt
+++ src/main/kotlin/com/mzet/uda/Util.kt
@@ -1,21 +1,25 @@
 package com.mzet.uda
 
-// Args
+// Cmd
 const val KEY = "key"
 const val DICTIONARY = "dict"
 const val RANDOM = "random"
+const val HELP = "help"
 const val EXIT = "exit"
 
 // Msg
 const val WELCOME = "Type one of the following command:"
 const val INCORRECT_INPUT = "Incorrect input"
-const val NO_KEY = "Specify rapidapi key"
-const val ON_KEY_SET = "Rapidapi key is set"
+const val NO_KEY = "Specify key from https://rapidapi.com/"
+const val ON_KEY_SET = "Key for https://rapidapi.com/ is set"
 const val ON_EXIT = "Shut down service"
 const val CONNECTION_LOST = "Something went wrong"
 
 val commandHelp = mapOf(
-    DICTIONARY to "Get specified word from dictionary",
+    KEY to "Specifiy your own key from https://rapidapi.com/",
+    DICTIONARY to "Get specified [word] from dictionary (you can specify [index] of definition if there are several)",
+    RANDOM to "Get random word definition",
+    HELP to "Get available command list",
     EXIT to "Shut down app"
 )
 
